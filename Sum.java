5e8f98328768307226a799970d2be0af6227429d
Niraj Kumar <neerajkumar89212@gmail.com>
2024-04-12 10:54:00 +0530

    add message in sum.java

--- Sum.java
+++ Sum.java
@@ -1,6 +1,6 @@
 public class Sum{
     public static void main (String args[]){
         int a=5; int b=6;
-        System.out.print(a+b);
+        System.out.print("The sum of two no are:" +a+b);
     }
 }
