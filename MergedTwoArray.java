e28324ed0107fa8c8b37410086a4d4d467f3eb38
Niraj Kumar <neerajkumar89212@gmail.com>
2024-04-26 02:05:52 +0530

    To Merge Sorted Array using Extra Space

---
+++ MergedTwoArray.java
@@ -0,0 +1,18 @@
+public class MergedTwoArray {
+    public static void main(String args[]){
+        int a[]={2,3,4};
+        int b[]={5,6,7,8,9};
+        int c[]=new int[a.length+b.length];
+        for(int i=0;i<a.length; i++){
+            c[i]=a[i];
+        }
+        for(int i=0; i<b.length; i++){
+            c[a.length+i]=b[i];
+        }
+        for(int i=0;i<c.length; i++){
+            System.out.print(c[i]+" ");
+
+        }
+    }
+    
+}
