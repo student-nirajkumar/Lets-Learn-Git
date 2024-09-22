c6ae513e887102d6d2be9449969206a5224bfdf1
Niraj Kumar <neerajkumar89212@gmail.com>
2024-09-22 15:08:28 +0530

    To print pattern

--- kthlargestelement.java
+++ kthlargestelement.java
@@ -1,6 +1,6 @@
 public class kthlargestelement {
     public static void main(String[] args) {
-        int a[] = {3,2,3,1,2,4,5,5,6};
+        int a[] = {1, 5, 2, 9, 3, 6};
         int k=4;
         for(int i=0; i<a.length-1; i++){
             for(int j=i+1;j<a.length;j++){
